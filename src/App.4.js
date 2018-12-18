ce78075fee1e76d7fd2b8032d85aa230cb4f9b96
이시형 <dltlgud1324@gmail.com>
2018-12-18 12:35:24 +0900

    test

--- src/App.4.js
+++ src/App.4.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import './App.css'
 
 class App extends Component{
   render(){
@@ -16,18 +15,11 @@ class App extends Component{
     };
     
     return(
-      <div className="my-div">
-        {/* 요소 밖에서는 이렇게 작성해요. */}
+      <div>
         <h1>리액트 안녕!</h1>
         <h2>{text}</h2>
         {condition && '보여주세요'}
-        <div style={style}
-        // self-closed 태그에서만 작동하는 주석
-        // 마지막 />가 꼭 새줄에 있어야 합니다.
-        /* 이렇게 작성할 수 도 있음 */
-        ></div>
-        // 여기 쓰는건 그대로 렌더링 됩니다.
-        /* 여기에선 주석 못써요 */
+        <div style={style}></div>
       </div>
     );
   }
